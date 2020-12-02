a55cdd4209d5c80fbd98dd0af0121dfaabb6ebea
syhue <sook.yan.hsy@gmail.com>
2020-12-02 22:11:37 +0800

    UPDATE FILES

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+include 'navbar.html';?>
+
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="" content="SitePoint">
+  <link rel="stylesheet" href="index.css">
+
+</head>
+
+<body>
+    <div id="intro" class="flex-container-vertical">
+        <div><h1>Good day.</h1></div>
+
+
+        <div class="flex-container-horizontal">
+            <div><img src="cute.jpg" title="Mike and Mocha"></div>
+            <div>
+            <p><br><br><a class="intro-passage">Have you take your breakfast?</a><br><a class="intro-passage">Are you ready for today?</a><br>*Image cited from "Milk and Mocha Bear"</p>
+            </div>
+        </div>
+
+
+        <div>
+            <!-- <div><img src="profile.webp" title="Mike and Mocha"></div> -->
+
+        </div>
+    </div>
+
+</body>
+
+
+</html>
---
+++ web-dev.php
@@ -0,0 +1,34 @@
+<?php
+include 'navbar.html';?>
+
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="" content="SitePoint">
+  <link rel="stylesheet" href="web-dev.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+</head>
+
+<body>
+    <div id="intro" class="flex-container-vertical">
+        <div><h1>Projects.</h1></div>
+
+
+        <div><a class="project-passage animate__shakeX" href="https://syhue.com/Progress-Report">1) Progress Report</a></div>
+
+
+        <div>
+            <!-- <div><img src="profile.webp" title="Mike and Mocha"></div> -->
+
+        </div>
+    </div>
+
+</body>
+
+
+</html>
