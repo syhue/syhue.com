c0bb19ef265e27d70909eabc3cfb45b0ddc9d021
syhue <sook.yan.hsy@gmail.com>
2020-12-04 07:12:02 +0800

    Update Files

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ include 'navbar.html';?>
 
 
             <div class="flex-container-horizontal small-size-vertical">
-                <div><img id="img-main" src="cute.jpg" title="Mike and Mocha"></div>
+                <div><img id="img-main" src="cute.jpg" alt="Mike and Mocha"></div>
 
                 <div>
                     <p ><br><br><a class="intro-passage">Have you take your breakfast?</a><br><a class="intro-passage">Are you ready for today?</a><br>*Image cited from "Milk and Mocha Bear"</p>
@@ -37,7 +37,7 @@ include 'navbar.html';?>
 
 
             <div class="flex-container-horizontal small-size-vertical">
-                <div><img id="img-main" src="cute.jpg" title="Mike and Mocha"></div>
+                <div><img id="img-main" src="cute.jpg" alt="Mike and Mocha"></div>
 
                 <div>
                     <p ><br><a class="intro-passage">Have you take your breakfast?</a><br><a class="intro-passage">Are you ready for today?</a><br>*Image cited from "Milk and Mocha Bear"</p>
