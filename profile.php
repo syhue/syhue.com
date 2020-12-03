6129f385f09ecfb76dc52318fe6fc96514771e5a
syhue <sook.yan.hsy@gmail.com>
2020-12-03 20:54:33 +0800

    Update Files

--- profile.php
+++ profile.php
@@ -18,23 +18,31 @@ include 'navbar.html';?>
     <div id="intro" class="flex-container-vertical">
         <div><h1>Profile.</h1></div>
 
+        <div><img id="formal-profile" src="formal-profile.jpg" alt="Profile Photo" width="100px"></div>
+
         <div>
+            <br>
             <h4>Hue Sook Yan</h4>
             <h5>Give Name is Sook Yan.</h5><h5>Surname is Hue.</h5>
             <h5>Born in 1995.</h5>
+            <br>
 
+            <h4>Education</h4>
+            <h5>Master of Science (Information Technology)</h5>
+            <h6>Present</h6>
+            <h6>Multimedia University</h6>
+            <h6>Cyberjaya, Selangor, Malaysia</h6>
+            <h6>Batch 2020/2021</h6>
+            <br>
 
-            <!-- <h5>Focused subjects:</h5></li>
-            <ul>
-                <li>PT3 - Mathematics</li>
-                <li>SPM - Modern Mathematics & Additional Mathematics</li>
-                <li>IGCSE - Modern Mathematics & Additional Mathematics</li>
-                <li>A Level - Mathematics</li>
-            </ul>
+            <h5>Bachelor of Science (Mathematics) with Honours</h5>
+            <h6>Graduated at 2020</h6>
+            <h6>Universiti Teknologi Malaysia</h6>
+            <h6>Skudai, Johor, Malaysia</h6>
+            <h6>Batch 2016/2017</h6>
 
-            <h5>Strong Mathematics foundations</h5>
-            <ul>
-                <li>Graduated from Mathematics degree with Honours at Universiti Teknologi Malaysia (UTM) Johor Bharu (JB)</li> -->
+            <br>
+
 
         </div>
 
