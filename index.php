bc15ac1aaf1da96ffead3e6ea62e195bfb3785af
syhue <sook.yan.hsy@gmail.com>
2020-12-03 13:29:18 +0800

    Update Files

--- index.php
+++ index.php
@@ -14,25 +14,71 @@ include 'navbar.html';?>
 </head>
 
 <body id=body-main>
-    <div id="intro" class="flex-container-vertical">
-        <div><h1 id=h1-main>Good day.</h1></div>
+
+    <section id="hide-on-mobile">
+        <div id="intro" class="flex-container-vertical">
+            <div><h1 id=h1-main>Good day.</h1></div>
 
 
-        <div class="flex-container-horizontal">
-            <div><img id="img-main" src="cute.jpg" title="Mike and Mocha"></div>
+            <div class="flex-container-horizontal small-size-vertical">
+                <div><img id="img-main" src="cute.jpg" title="Mike and Mocha"></div>
+
+                <div>
+                <p ><br><br><a class="intro-passage">Have you take your breakfast?</a><br><a class="intro-passage">Are you ready for today?</a><br>*Image cited from "Milk and Mocha Bear"</p>
+                </div>
+            </div>
+
+
             <div>
-            <p><br><br><a class="intro-passage">Have you take your breakfast?</a><br><a class="intro-passage">Are you ready for today?</a><br>*Image cited from "Milk and Mocha Bear"</p>
+                <!-- <div><img src="profile.webp" title="Mike and Mocha"></div> -->
+
             </div>
         </div>
+    </section>
 
 
-        <div>
-            <!-- <div><img src="profile.webp" title="Mike and Mocha"></div> -->
+    <section id="hide-on-pc">
+        <div id="intro" class="flex-container-vertical">
+            <div><h1 id=h1-main>Good day.</h1></div>
 
+
+            <div class="flex-container-horizontal small-size-vertical">
+                <div><img id="img-main" src="cute.jpg" title="Mike and Mocha"></div>
+
+                <div>
+                <p ><br><a class="intro-passage">Have you take your breakfast?</a><br><a class="intro-passage">Are you ready for today?</a><br>*Image cited from "Milk and Mocha Bear"</p>
+                </div>
+            </div>
+
+
+            <div>
+                <!-- <div><img src="profile.webp" title="Mike and Mocha"></div> -->
+
+            </div>
         </div>
-    </div>
+    </section>
 
 </body>
+
+<script type="text/javascript">
+    document.getElementByClassName("big-screen").onclick = function() {
+
+        document.getElementById("circle").style.display = "none";
+
+    }
+
+    document.getElementById("rounded").onclick = function() {
+
+        document.getElementById("rounded").style.display = "none";
+
+    }
+
+    document.getElementById("square").onclick = function() {
+
+        document.getElementById("square").style.display = "none";
+
+    }
+</script>
 
 
 </html>
