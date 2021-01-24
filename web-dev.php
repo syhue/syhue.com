3788823f4b9958b9f81c53a655070cecec29e639
syhue <sook.yan.hsy@gmail.com>
2021-01-25 07:27:13 +0800

    update files

--- web-dev.php
+++ web-dev.php
@@ -1,10 +1,11 @@
 <?php
-include 'navbar.html';?>
+include 'navbar.html'; ?>
 
 
 <!doctype html>
 
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" href="web-dev.css">
@@ -14,35 +15,45 @@ include 'navbar.html';?>
 </head>
 
 <body>
-    <div id="intro" class="flex-container-vertical">
-        <div><h1>Projects.</h1></div>
-
-
-        <div class="tooltip"><a class="project-passage animate__shakeX" href="https://syhue.com/progress-report">1) Progress Report </a><span class="tooltiptext">  This works done during my master studies.</span>
-        </div>
-
-
-
-
-        <div class="tooltip"><a class="project-passage animate__shakeX" href="https://syhue.com/csv-database-convertor">2) CSV to Database Convertor </a><span class="tooltiptext">  This works done during my internship in MIMOS Berhad.</span>
-        </div>
-
-        <!-- <div class="image_slide">
-        <section class="hide-in-mobile">
-
-            <img class="images" src="progress-photo1.webp">
-            <img class="images" src="progress-photo2.webp">
-            <img class="images" src="progress-photo3.webp">
-
-            <i id="leftarrow" class='far fa-hand-point-left' style='font-size:36px' onclick="plusDivs(-1)"></i>
-            <i id="rightarrow" class='far fa-hand-point-right' style='font-size:36px' onclick="plusDivs(+1)"></i>
-        </section>
-        </div> -->
-
-
-
+  <div id="intro" class="flex-container-vertical">
+    <div>
+      <h1>Projects.</h1>
     </div>
-<script>
+
+
+    <div class="tooltip">
+      <a class="project-passage animate__shakeX" href="https://syhue.com/progress-report">
+        1) Progress Report 
+      </a>
+      <span class="tooltiptext">
+        HTML, CSS, PHP, MySql Languages are used.
+      </span>
+    </div>
+
+    <div class="tooltip">
+      <a class="project-passage animate__shakeX" href="https://syhue.com/csv-database-convertor">
+       2) CSV to Database Convertor 
+      </a>
+      <span class="tooltiptext"> 
+        HTML, CSS, PHP, MySql Languages are used.
+      </span>
+    </div>
+    
+    <div class="tooltip">
+      <a class="project-passage animate__shakeX" href="https://syhue.com/angular-material-table">
+        3) Mini Project involved Angular Framework 
+      </a>
+      <span class="tooltiptext">
+        Basic usage of applying directives, services, angular materials. 
+      </span>
+    </div>
+
+
+
+
+
+  </div>
+  <script>
     var slideIndex = 1;
     showDivs(slideIndex);
 
@@ -53,14 +64,18 @@ include 'navbar.html';?>
     function showDivs(n) {
       var i;
       var x = document.getElementsByClassName("images");
-      if (n > x.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = x.length} ;
+      if (n > x.length) {
+        slideIndex = 1
+      }
+      if (n < 1) {
+        slideIndex = x.length
+      };
       for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
       }
-      x[slideIndex-1].style.display = "block";
+      x[slideIndex - 1].style.display = "block";
     }
-</script>
+  </script>
 
 
 </body>
